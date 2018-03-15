4794994d97f92b3e4fb40f1efef6a30e2edf8f9b
gxx <562606139@qq.com>
2018-03-15 22:57:25 +0800

    0315

---
+++ 0315/Dictionary.js
@@ -0,0 +1,40 @@
+class Dictionary{
+	constructor(){
+		this.obj = {};
+	}
+	set(key,val){
+		this.obj[key] = val
+	}
+	delete(key){
+		if(this.has(key)){
+			delete this.obj[key]
+			return true
+		}
+		return false
+	}
+	has(key){
+		return key in this.obj
+	}
+	get(key){
+		return this.has(key)
+		?
+		this.obj[key]
+		:
+		undefined
+	}
+	clear(){
+		this.obj = {}
+	}
+	size(){
+		return Object.keys(this.obj).length
+	}
+	keys(){
+		return Object.keys(this.obj)
+	}
+	values(){
+		return Object.values(this.obj)
+	}
+	getItems(){
+		return this.obj
+	}
+}
