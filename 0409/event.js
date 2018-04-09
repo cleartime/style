295132edd981325eb99b23399ffd0cb28055e460
gxx <562606139@qq.com>
2018-04-09 23:45:32 +0800

    事件机制

--- 0409/event.js
+++ 0409/event.js
@@ -1,6 +1,7 @@
-// 这个是最简单的，但是只能触发一个
 class EventBus{
 	constructor(){
+		this.a = 1
+		this.b = 2
 		// 用map来存储key和value
 		this.events = new Map();
 	}
@@ -12,17 +13,45 @@ class EventBus{
 	}
 	// 触发这个event
 	// 触发的类型
-	emit(type){
+	// 传递的参数
+	emit(type, ...arr){
+		console.log(this)
 		// 判断在不在这个里面，如果再就取这个值，因为这个值是个回调函数，就可以直接调用
 		if(this.events.has(type)){
-			this.events.get(type)()
+			// 有参数用apply
+			if(!arr.length){
+				this.events.get(type).call(this)
+			}else{
+				this.events.get(type).apply(this,arr)
+			}
 		}
 	}
 }
-
+var a = 2
 const e = new EventBus();
 
-e.on('abc',function(){
-	alert(1)
+e.on('abc',function(res){
+	console.log(res)
+	// alert(1)
 })
-e.emit('abc')
+e.emit('abc',2324)
+
+
+e.on('def',function(){
+	console.log(this.a)
+	// alert(1)
+})
+
+e.emit('def',function(){
+	console.log(this.b)
+})
+
+
+const event = new EventBus();
+
+e.on('abc',function(res){
+	console.log(res)
+	// alert(1)
+})
+
+e.emit('abc',534)
