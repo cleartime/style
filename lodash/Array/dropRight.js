9bbfe135eaf4a41e6aa514afb76fe43bf021dbbe
gxx <562606139@qq.com>
2018-09-14 14:16:44 +0800

    drop

---
+++ lodash/Array/dropRight.js
@@ -0,0 +1,45 @@
+/**
+ * 创建一个数组切片，其中包含从末尾删除的n个元素。
+ * 例子如下
+ */
+
+_.dropRight([1, 2, 3]);
+// => [1, 2]
+ 
+_.dropRight([1, 2, 3], 2);
+// => [1]
+ 
+_.dropRight([1, 2, 3], 5);
+// => []
+ 
+_.dropRight([1, 2, 3], 0);
+// => [1, 2, 3]
+
+
+// 老规矩，自己先实现一个
+// 思路就是用slice方法去根据第二个参数来切割
+
+function _dropRight(arr, size = arr.length) {
+  const len = arr == null ? 0 : arr.length;
+  return arr.slice(0,size)
+}
+log(dropRight([1, 2, 3]))
+
+
+// 看源码
+function drop(array, n=1) {
+  // 获取长度
+  const length = array == null ? 0 : array.length
+  // 没有长度返回空数组
+  return length
+    ? slice(array, n < 0 ? 0 : n, length) // 此slice传入三个参数(数组，切割的起始点，切割的长度)
+    : []
+}
+
+
+/**
+ * 总结
+ * 1. 参数默认值代替函数体内做判断
+ * 2. 获取长度要判断null值
+ * 3. 返回数组的时候要懒得length，没有的话要返回空数组
+ */
